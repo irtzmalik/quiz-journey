38a3f081e3cc68d2918ca1a3c94d586496dd6780
Winson Quon <winguy2003@gmail.com>
2020-01-14 21:51:19 -0500

    add getQuestions controller

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,11 @@
+const controllers = require('../controllers/index');
 const router = require('express').Router();
 
 router.get('/', (req, res) => res.json('Sample API get endpoint'));
+
+router.get('/questions/:category/:difficulty', (req, res) => {
+    let data = controllers.getQuestions(req.params.category, req.params.difficulty);
+    res.json(data);
+});
 
 module.exports = router;
