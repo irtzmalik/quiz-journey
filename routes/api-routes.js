76ce5e2fbac009f1f0e6003a5ba904c05ba424c9
Irtaza Malik <irtz.malik@gmail.com>
2020-01-16 19:49:45 -0500

    having issues in routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
+const db = require('../models');
 
-router.get('/', (req, res) => res.json('Sample API get endpoint'));
 
+//router.get('/', (req, res) => res.json('Sample API get endpoint'));
+router.get('/characters', function(req, res) {
+   // console.log(Characters.findAll());
+   db.Characters.findAll().then(function(result) {
+       console.log(res.json(result));
+    return res.json(result);
+  });
+});
 module.exports = router;
