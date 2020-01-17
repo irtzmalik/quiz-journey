32253731220aca1ff4c67c42637bcf6b3f7b2bbe
Brad <bkarulas@outlook.com>
2020-01-17 10:52:47 -0500

    Sequelize api route for get users

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 const controllers = require('../controllers/index');
 const router = require('express').Router();
+const db = require("../models");
 
 router.get('/', (req, res) => res.json('Sample API get endpoint'));
 
@@ -10,5 +11,18 @@ router.get('/questions/:category/:difficulty', (req, res) => {
         res.json(result);
     });
 });
+
+router.get('/token/:token', function(req, res) {
+    console.log(req.params.token);
+    db.Users.findOne({
+      where: {
+        passcode: req.params.token
+      }
+    })
+      .then(function(result) {
+        res.json(result);
+      });
+  });
+
 
 module.exports = router;
