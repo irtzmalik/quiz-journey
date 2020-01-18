1f07221e788df28dacf4a2f0d25131d2960a5393
Winyumi <winguy2003@hotmail.com>
2020-01-18 16:51:48 -0500

    redirect if token not found

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,17 +2,21 @@ const router = require('express').Router();
 const exphbs = require("express-handlebars");
 
 router.get('/', (req, res) => {
-  //res.sendFile(path.join(__dirname, '../public/index.html'));
   res.render("index");
 });
 
 
 router.get('/:token', (req, res) => {
-  //res.sendFile(path.join(__dirname, '../public/index.html'));
-  res.render("game", {
-    name: "Winson",
-    token: "43n895n4y39432"
-  });
+  if (req.params.token === "43n895n4y39432") {
+    let account = {
+      id: 1,
+      name: "Winson",
+      token: "43n895n4y39432"
+    };
+    res.render("game", account);
+  } else {
+    res.redirect('/');
+  }
 });
 
 // Place this route below all others to send he index.html file
