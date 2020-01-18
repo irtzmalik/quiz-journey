e33999b1f6fc7c2c37afb493eece66915bd1ae2e
Winyumi <winguy2003@hotmail.com>
2020-01-18 16:20:33 -0500

    basic handlebars setup

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,14 +1,26 @@
-const path = require('path');
 const router = require('express').Router();
+const exphbs = require("express-handlebars");
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  //res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.render("index");
+});
+
+
+router.get('/:token', (req, res) => {
+  //res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.render("game", {
+    name: "Winson",
+    token: "43n895n4y39432"
+  });
 });
 
 // Place this route below all others to send he index.html file
 // to any request that is not explicitly defined above
+/*
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+*/
 
 module.exports = router;
