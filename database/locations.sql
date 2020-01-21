ade702c79e51ff16853858939dda3e812bb56094
Winyumi <winguy2003@hotmail.com>
2020-01-21 00:50:47 -0500

    add timestamps to locations.sql

--- database/locations.sql
+++ database/locations.sql
@@ -1,6 +1,6 @@
 USE quiz_journey;
-INSERT INTO Locations(name,image, category) VALUES
-('Earth Science', 'image', 22),
-('Cinema','image',11),
-('Rock n Roll', 'image',12),
-('Electronic Game','image',15);
+INSERT INTO Locations (name, image, category, createdAt, updatedAt) VALUES
+('Earth Science', 'image', 22, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Cinema', 'image', 11, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Rock n Roll', 'image', 12, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Electronic Game', 'image', 15, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
