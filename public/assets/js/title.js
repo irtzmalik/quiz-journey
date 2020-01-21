3f1c9119e13e33d20e2d2d375f6737210f6221ed
Winyumi <winguy2003@hotmail.com>
2020-01-21 00:42:11 -0500

    update title screen

--- public/assets/js/title.js
+++ public/assets/js/title.js
@@ -2,29 +2,48 @@
 
 const init = () => {
 
+    const scene = $('#game');
+
+    const renderTitleScreen = () => {
+        scene.empty();
+        scene.append(`
+            <div class="jumbotron text-center">
+                <h1>Quiz Journey</h1>
+            </div>
+            <div class="text-center">
+                <button id="btnNewGame" class="btn btn-primary">New Game</button>
+                <button id="btnLoadGame" class="btn btn-primary">Load Game</button>
+            </div>
+        `);
+        $('#btnNewGame').click(newGame);
+        $('#btnLoadGame').click(loadGame);
+    };
+
     const newGame = () => {
-        let name = prompt("Enter your name");
+        let name = prompt("Enter your name").trim();
+        if (!name) return alert("Please enter a name.");
         $.post("/api/users", { name: name })
         .done(result => {
             location.href = `/${result.token}`;
         })
         .fail(() => {
-            alert("error");
-        })
+            alert("Error: Unable to create user.");
+        });
     };
 
     const loadGame = () => {
-        let token = prompt("Enter your save code");
+        let token = prompt("Enter your save code").trim();
+        if (!token) return alert("Game not loaded.");
         $.get(`/api/token/${token}`)
         .done(result => {
-            location.href = `/${result.token}`;
+            result ? location.href = `/${result.token}`
+            : alert("Saved game not found.");
         })
         .fail(() => {
-            alert("error");
-        })
+            alert("Error: Unable to fetch data.");
+        });
     };
 
-    $('#btnNewGame').click(newGame);
-    $('#btnLoadGame').click(loadGame);
+    renderTitleScreen();
 
 };
