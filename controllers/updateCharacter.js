6efe779d0065ecf4777c1523b04406f7d9eb22f3
Anya <anyaanto1909@gmail.com>
2020-01-21 10:08:30 -0500

    added updateCharacter controller & put route

--- controllers/updateCharacter.js
+++ controllers/updateCharacter.js
@@ -1,4 +1,4 @@
-module.exports = function createCharacter(user_id, points) {
+module.exports = function updateCharacter(user_id, points) {
   const db = require('../models/index');
   return db.Characters.update({ points: points }, {
     where: {
