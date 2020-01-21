7eb42612a5bfe2932827e2b80e8a3b96b2cf248e
Winyumi <winguy2003@hotmail.com>
2020-01-21 05:11:18 -0500

    use category words

--- database/locations.sql
+++ database/locations.sql
@@ -1,6 +1,6 @@
 USE quiz_journey;
 INSERT INTO Locations (name, image, category, createdAt, updatedAt) VALUES
-('Earth Science', 'image', 22, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Cinema', 'image', 11, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Rock n Roll', 'image', 12, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-('Electronic Game', 'image', 15, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+('Earth Science', '/assets/img/mountains.png', 'geography', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Cinema', '/assets/img/mountains.png', 'film', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Rock n Roll', '/assets/img/mountains.png', 'music', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+('Electronic Game', '/assets/img/mountains.png', 'videogames', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
