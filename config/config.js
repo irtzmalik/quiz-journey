1ef2eb4e57aa50a60d6aea72a78060747715f77b
Winson Quon <winguy2003@gmail.com>
2020-01-23 13:48:52 -0500

    add env to travis yml

--- config/config.js
+++ config/config.js
@@ -9,9 +9,9 @@ module.exports = {
     dialect: 'mysql',
   },
   test: {
-    username: "root",
-    password: "",
-    database: "quiz_journey",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql',
   },
